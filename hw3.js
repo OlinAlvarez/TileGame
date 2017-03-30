7649c6db95a35a4af6b509d6e0a6c02f2ec3a477
Olin Alvarez <olinalvarez@gmail.com>
2017-03-30 14:47:38 -0700

    added list of tasks

--- hw3.js
+++ hw3.js
@@ -62,47 +62,5 @@ function generateGrid(grid){
             temp.push(new Tile(i,j));
         }
         grid.push(temp);
-    }
-
-    grid.map(function(row,grid){
-        row.map(function(tile,grid){
-            if(tile.x === 0){
-                tile.north = null;
-            }
-            if(tile.y === 0){
-                tile.northwest = null;
-                tile.southwest = null;
-            }
-            if(tile.x === 7){
-                tile.northeast = null;
-                tile.southeast = null;
-            }
-            if(tile.y ===7){
-                tile.south = null;
-            }
-            if(tile.x > 0 || tile.y >0){
-
-                
-                if(tile.north != null){
-                    tile.north = grid[tile.x][tile.y  - 1];
-                }
-                if(tile.south != null){
-                    tile.south = grid[tile.x][tile.y+1];
-                }
-                if(tile.northeast != null){
-                    tile.northeast = grid[tile.x-1][tile.y+1];
-                }
-                if(tile.northwest != null){
-                    tile.northwest = grid[tile.x-1][tile.y-1];
-                }
-                if(tile.southwest != null){
-                    tile.southwest = grid[tile.x+1][tile.y-1];
-                }
-                if(tile.southeast != null){
-                    tile.southeast = grid[tile.x+1][tile.y+1];
-                }
-            }
-        });
-    });
-    
+    }       
 }
